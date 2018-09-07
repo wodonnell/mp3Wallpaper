5f309f8c5d5d7432f082f95aa057e403a1912e18
Wayne O'Donnell <wayne.odonnell@ntlworld.com>
2018-09-07 16:44:18 +0100

    add collage function

--- app/src/main/java/com/wayneodonnell/mp3wallpaper/MainActivity.java
+++ app/src/main/java/com/wayneodonnell/mp3wallpaper/MainActivity.java
@@ -6,9 +6,17 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaMetadataRetriever;
 import android.os.Environment;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.renderscript.ScriptIntrinsicResize;
+import android.renderscript.Type;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +26,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.renderscript.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -26,6 +35,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -38,8 +48,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.btnSetWallpaper)  Button mBtnSetWallpaper;
     @BindView(R.id.btnRandom)  Button mBtnRandom;
     @BindView(R.id.imageView) ImageView mImageView;
+    @BindView(R.id.btnCollage)  Button mBtnCollage;
 
-    ArrayList<String> fileList=new ArrayList<String>();
+    ArrayList<String> mFileList=new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,18 +61,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         /* Set onClick listeners */
         mBtnSetWallpaper.setOnClickListener(this);
         mBtnRandom.setOnClickListener(this);
+        mBtnCollage.setOnClickListener(this);
         //Get list of files when app started
         getFileList();
     }
 
     @Override
     public void onClick(View v){
+        //TODO - Create a random collage
         if(v==mBtnSetWallpaper){
             /* Clear the image */
             //mImageView.setImageResource(0);
             setPaper(mImageView);
             Toast.makeText(MainActivity.this,"Wallpaper set",Toast.LENGTH_LONG).show();
         }
+        if(v==mBtnCollage)
+        {
+            mImageView.setImageBitmap(createCollage());
+        }
+
         if(v==mBtnRandom){
             String filepath=getFilePath();
 
@@ -79,8 +97,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void getFileList() {
         //Before accessing files, need to check permission granted
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
-            //Need to request permission
+            // If permission is not granted need to request permission
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     Constants.PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
@@ -88,36 +105,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         List<File> files = getListFiles(new File("/storage/C219-D78B/Music/"));
 
-        //Add each file name to the list
+        //Add each file, add name to the list
         for(File file: files){
-            fileList.add(file.toString());
+            mFileList.add(file.toString());
         }
-
     }
 
     public String getFilePath(){
+        //TODO - Find actual path to SDCard
         //String sdpath = Environment.getExternalStorageDirectory().getAbsolutePath();
         //String sdpath= System.getenv("EXTERNAL_STORAGE");
         //String sdpath="/storage/C219-D78B";  //Hardcoded path - need to work out how to find this!
         //String sdpath="/storage/emulated/0/Download";  //Hardcoded path - need to work out how to find this!
         String filepath="";
-
-        //Create a list of some files to read - eventually this will read through the SD card
-
-
-        /*fileList.add("/storage/C219-D78B/Music/Bon Jovi/Keep The Faith/03 I'll Sleep When I'm Dead.mp3");
-        fileList.add("/storage/C219-D78B/Music//Cast/All Change/03 Sandstorm.mp3");
-        fileList.add("/storage/C219-D78B/Music//The Police/The Very Best Of Sting And The Police/03 Englishman In New York.mp3");
-        fileList.add("/storage/C219-D78B/Music//Slade/Sladest/01 Cum On Feel The Noize.mp3");
-        fileList.add("/storage/C219-D78B/Music//Queen/Hot Space/09 Las Palabras De Amor (The Words O.mp3");
-*/
         //Pick one of the files at random
-        if(fileList.size()>0){
+        if(mFileList.size()>1){
             Random rand = new Random();
-            int rnd = rand.nextInt(fileList.size() - 1);
+            int rnd = rand.nextInt(mFileList.size() - 1);
 
             //Pick a random file
-            filepath = fileList.get(rnd);
+            filepath = mFileList.get(rnd);
         }
         return filepath;
     }
@@ -152,7 +159,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private List<File> getListFiles(File parentDir) {
-        List<File> inFiles = new ArrayList<>();
+        //Find only mp3 files
+        Boolean alreadyAdded=false;
+  /*      List<File> inFiles = new ArrayList<>();
         Queue<File> files = new LinkedList<>();
         files.addAll(Arrays.asList(parentDir.listFiles()));
         while (!files.isEmpty()) {
@@ -160,9 +169,101 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (file.isDirectory()) {
                 files.addAll(Arrays.asList(file.listFiles()));
             } else if (file.getName().endsWith(".mp3")) {
-                inFiles.add(file);
+                //Only add if a file from this folder has not already been added
+                if(!alreadyAdded) {
+                    alreadyAdded=true;
+                    inFiles.add(file);
+                }
             }
         }
+
+*/
+        ArrayList<File> inFiles = new ArrayList<File>();
+        File[] files = parentDir.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                inFiles.addAll(getListFiles(file));
+            } else {
+                if(file.getName().endsWith(".mp3")){
+                    if(!alreadyAdded) {
+                        alreadyAdded=true;
+                        inFiles.add(file);
+                    }
+                }
+            }
+        }
+
         return inFiles;
+    }
+
+    private Bitmap createCollage(){
+        // TODO - Creates a collage of 16 bitmaps
+        // TODO - Collage needs to resize images so all same size
+        //Get bitmaps
+        String[] collagelist=new String[16];
+
+        //Shuffle the collection
+        Collections.shuffle(mFileList,new Random());
+
+        Bitmap[] collagebitmaps=new Bitmap[16];
+
+        //Because mFileList is shuffled, we can just take the first 16
+        for(int i=0;i<16;i++){
+            //ScaledBitmap not very good quality
+            //collagebitmaps[i]= Bitmap.createScaledBitmap(extractAlbumArt(collagelist[i]),100,100,false);
+            RenderScript RS = RenderScript.create(this);
+            collagebitmaps[i]= resizeBitmap(RS,extractAlbumArt(mFileList.get(i)),100);
+        }
+
+        Bitmap result = Bitmap.createBitmap(collagebitmaps[0].getWidth() * 4, collagebitmaps[0].getHeight() * 4, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(result);
+        Paint paint = new Paint();
+        for (int i = 0; i < collagebitmaps.length; i++) {
+            canvas.drawBitmap(collagebitmaps[i], collagebitmaps[i].getWidth() * (i % 4), collagebitmaps[i].getHeight() * (i / 4), paint);
+        }
+        return result;
+    }
+    public static Bitmap resizeBitmap(RenderScript rs, Bitmap src, int dstWidth) {
+        Bitmap.Config  bitmapConfig = src.getConfig();
+        int srcWidth = src.getWidth();
+        int srcHeight = src.getHeight();
+        float srcAspectRatio = (float) srcWidth / srcHeight;
+        int dstHeight = (int) (dstWidth / srcAspectRatio);
+
+        float resizeRatio = (float) srcWidth / dstWidth;
+
+        /* Calculate gaussian's radius */
+        float sigma = resizeRatio / (float) Math.PI;
+        // https://android.googlesource.com/platform/frameworks/rs/+/master/cpu_ref/rsCpuIntrinsicBlur.cpp
+        float radius = 2.5f * sigma - 1.5f;
+        radius = Math.min(25, Math.max(0.0001f, radius));
+
+        /* Gaussian filter */
+        Allocation tmpIn = Allocation.createFromBitmap(rs, src);
+        Allocation tmpFiltered = Allocation.createTyped(rs, tmpIn.getType());
+        ScriptIntrinsicBlur blurInstrinsic = ScriptIntrinsicBlur.create(rs, tmpIn.getElement());
+
+        blurInstrinsic.setRadius(radius);
+        blurInstrinsic.setInput(tmpIn);
+        blurInstrinsic.forEach(tmpFiltered);
+
+        tmpIn.destroy();
+        blurInstrinsic.destroy();
+
+        /* Resize */
+        Bitmap dst = Bitmap.createBitmap(dstWidth, dstHeight, bitmapConfig);
+        Type t = Type.createXY(rs, tmpFiltered.getElement(), dstWidth, dstHeight);
+        Allocation tmpOut = Allocation.createTyped(rs, t);
+        ScriptIntrinsicResize resizeIntrinsic = ScriptIntrinsicResize.create(rs);
+
+        resizeIntrinsic.setInput(tmpFiltered);
+        resizeIntrinsic.forEach_bicubic(tmpOut);
+        tmpOut.copyTo(dst);
+
+        tmpFiltered.destroy();
+        tmpOut.destroy();
+        resizeIntrinsic.destroy();
+
+        return dst;
     }
 }
