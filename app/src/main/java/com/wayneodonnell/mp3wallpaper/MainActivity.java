28a407581d64b02de09f7d74b70900df7a067403
Wayne O'Donnell <wayne.odonnell@ntlworld.com>
2018-09-07 13:06:01 +0100

    basic functionality to randomly fimd album cover and set as wallpaper

--- app/src/main/java/com/wayneodonnell/mp3wallpaper/MainActivity.java
+++ app/src/main/java/com/wayneodonnell/mp3wallpaper/MainActivity.java
@@ -1,10 +1,12 @@
 package com.wayneodonnell.mp3wallpaper;
 
 import android.Manifest;
+import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaMetadataRetriever;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
@@ -20,6 +22,13 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Random;
 
 import butterknife.BindView;
@@ -30,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.btnRandom)  Button mBtnRandom;
     @BindView(R.id.imageView) ImageView mImageView;
 
+    ArrayList<String> fileList=new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +50,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         /* Set onClick listeners */
         mBtnSetWallpaper.setOnClickListener(this);
         mBtnRandom.setOnClickListener(this);
+        //Get list of files when app started
+        getFileList();
     }
+
     @Override
     public void onClick(View v){
         if(v==mBtnSetWallpaper){
@@ -52,19 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String filepath=getFilePath();
 
             if(filepath!="") {
-                /*
-                File imageFile = new File(filepath); //Define actual file
-                FileInputStream fis = null;  //Create a filestream
-                // Check file exists and if so, populate filestream
-                try {
-                    fis = new FileInputStream(imageFile);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                //Create a bitmap using the filestream
-                Bitmap bm = BitmapFactory.decodeStream(fis);
-*/
+                //Get album art from mp3 file
                 Bitmap bm=extractAlbumArt(filepath);
                 //Set ImageView to use bitmap
                 if(bm!=null){
@@ -74,13 +76,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
-    public String getFilePath(){
-        //String sdpath = Environment.getExternalStorageDirectory().getAbsolutePath();
-        //String sdpath= System.getenv("EXTERNAL_STORAGE");
-        //String sdpath="/storage/C219-D78B";  //Hardcoded path - need to work out how to find this!
-        //String sdpath="/storage/emulated/0/Download";  //Hardcoded path - need to work out how to find this!
-        String filepath="";
-
+    public void getFileList() {
         //Before accessing files, need to check permission granted
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             // Permission is not granted
@@ -89,21 +85,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                     Constants.PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
         }
-        //Pick one of the 2 files at random
-        //Random rand=new Random();
-        //int rnd = rand.nextInt(10) + 1;
-        //if(rnd<=5){
-//            filepath="/storage/emulated/0";
 
-        //}
-        //else{
-            filepath="/storage/C219-D78B/Music/Bon Jovi/Keep The Faith/03 I'll Sleep When I'm Dead.mp3";
-  //      }
+        List<File> files = getListFiles(new File("/storage/C219-D78B/Music/"));
+
+        //Add each file name to the list
+        for(File file: files){
+            fileList.add(file.toString());
+        }
+
+    }
+
+    public String getFilePath(){
+        //String sdpath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        //String sdpath= System.getenv("EXTERNAL_STORAGE");
+        //String sdpath="/storage/C219-D78B";  //Hardcoded path - need to work out how to find this!
+        //String sdpath="/storage/emulated/0/Download";  //Hardcoded path - need to work out how to find this!
+        String filepath="";
+
+        //Create a list of some files to read - eventually this will read through the SD card
+
+
+        /*fileList.add("/storage/C219-D78B/Music/Bon Jovi/Keep The Faith/03 I'll Sleep When I'm Dead.mp3");
+        fileList.add("/storage/C219-D78B/Music//Cast/All Change/03 Sandstorm.mp3");
+        fileList.add("/storage/C219-D78B/Music//The Police/The Very Best Of Sting And The Police/03 Englishman In New York.mp3");
+        fileList.add("/storage/C219-D78B/Music//Slade/Sladest/01 Cum On Feel The Noize.mp3");
+        fileList.add("/storage/C219-D78B/Music//Queen/Hot Space/09 Las Palabras De Amor (The Words O.mp3");
+*/
+        //Pick one of the files at random
+        if(fileList.size()>0){
+            Random rand = new Random();
+            int rnd = rand.nextInt(fileList.size() - 1);
+
+            //Pick a random file
+            filepath = fileList.get(rnd);
+        }
         return filepath;
     }
 
-    public void setPaper(ImageView imageview){
-
+    public void setPaper(ImageView imageView){
+        //Set wallpaper to content of imageView
+        WallpaperManager myWallpaperManager
+                = WallpaperManager.getInstance(getApplicationContext());
+        try {
+            BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+            myWallpaperManager.setBitmap(drawable.getBitmap());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Bitmap extractAlbumArt(String filename){
@@ -121,5 +149,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return bm;  //Return the bitmap
+    }
+
+    private List<File> getListFiles(File parentDir) {
+        List<File> inFiles = new ArrayList<>();
+        Queue<File> files = new LinkedList<>();
+        files.addAll(Arrays.asList(parentDir.listFiles()));
+        while (!files.isEmpty()) {
+            File file = files.remove();
+            if (file.isDirectory()) {
+                files.addAll(Arrays.asList(file.listFiles()));
+            } else if (file.getName().endsWith(".mp3")) {
+                inFiles.add(file);
+            }
+        }
+        return inFiles;
     }
 }
